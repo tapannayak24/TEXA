3e3754590185112fdb7a978b9ee159fe8e9f709b
tapannayak24 <tapanofficial888@gmail.com>
2022-08-12 17:23:49 +0530

    final project

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// CAROUSEL STARTS
+$('.p-carousel').owlCarousel({
+    loop:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
